b4328d8e22ef99c2fb7b123ec7e12f341910f6fa
Nithish H <87592303+Drsmith@users.noreply.github.com>
2025-05-27 12:21:30 +0530

    Update TermsPage.jsx

--- server/client/src/components/TermsPage.jsx
+++ server/client/src/components/TermsPage.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./TermsPage.css";
 import TopNavbar from "../components/TopNavbar";
 
 const TermsPage = () => {
   const [lang, setLang] = useState("en");
   const [termsContent, setTermsContent] = useState("");
+  const navigate = useNavigate();
 
- useEffect(() => {
-  fetch(`https://terms-products-app.onrender.com/terms?lang=${lang}`)
-    .then((res) => res.json())
-    .then((data) => setTermsContent(data.content))
-    .catch((err) => console.error("Error fetching terms:", err));
-}, [lang]);
+  useEffect(() => {
+    fetch(`https://terms-products-app.onrender.com/terms?lang=${lang}`)
+      .then((res) => res.json())
+      .then((data) => setTermsContent(data.content))
+      .catch((err) => console.error("Error fetching terms:", err));
+  }, [lang]);
 
   return (
     <div className="terms-page">
@@ -19,10 +21,9 @@ const TermsPage = () => {
 
       <h1 className="terms-heading">Terms</h1>
 
-      <button className="back-button" onClick={() => window.location.href = "/"}>
-  Close and Go Back
-</button>
-
+      <button className="back-button" onClick={() => navigate("/")}>
+        Close and Go Back
+      </button>
 
       <div className="terms-box">
         <div
