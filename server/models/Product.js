32ff74bb6f5c54d04c1fc9fd6e340589b130356b
Nithish H <87592303+Drsmith@users.noreply.github.com>
2025-05-27 11:06:52 +0530

    Update Product.js

--- server/models/Product.js
+++ server/models/Product.js
@@ -2,13 +2,40 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Product = sequelize.define('Product', {
-  articleNo: DataTypes.STRING,
-  productName: DataTypes.STRING,
-  inPrice: DataTypes.INTEGER,
-  price: DataTypes.INTEGER,
-  unit: DataTypes.STRING,
-  inStock: DataTypes.INTEGER,
-  description: DataTypes.STRING,
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  articleNo: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  productName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  inPrice: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  unit: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  inStock: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
 });
 
 module.exports = Product;
